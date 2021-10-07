85d989f7d68f06bcefbc2ebe8b7adb5293b2b00e
ivvz <ivanwzh.iqm@gmail.com>
2021-10-07 17:04:46 -0500

    se cambio el archivo mensajes, se hizo un cambio en la funcion pausa

--- simulador_cilindro/mensajes_error.py
+++ simulador_cilindro/mensajes_error.py
@@ -1,37 +1,37 @@
 import time
-
-def err_valor_numerico():
-    print(' ***ERROR*** \nTu tipo de dato no es válido \nDebes ingresar solo caracteres numericos.')
+  
+def pausa(tiempo=0.5):
+    time.sleep(tiempo)
     
-def loading_text(texto):
-    for i in range(len(texto)):
-        print(texto[i], sep=' ', end=' ', flush=True)
-        time.sleep(0.2)
+def loading_text(texto,tiempo=0.1):
+    for i in texto:
+        print(i, end=' ',flush=True)
+        time.sleep(tiempo) 
+        
+def mensaje_error():
+    loading_text('\n***** ERROR *****\n')
+    pausa()
 
-def error_msj():
-    print('\n***** ERROR *****\n')
-    time.sleep(1)
+def error_valor_numerico():
+    mensaje_error()
+    loading_text('\nTu tipo de dato no es válido \nDebes ingresar solo caracteres numericos.\n')
 
 def zero_error():
-    error_msj()
-    print('\nEl valor no puede ser cero')
-    time.sleep(1)
-    print('\nIntenta nuevamente')
-    time.sleep(1)
+    mensaje_error()
+    loading_text('\nEl valor no puede ser cero\n')
+    pausa()
+    loading_text('\n***** Intenta nuevamente *****\n')
+    pausa()
 
 def numero_negativo_invalido():
-    error_msj()
-    print('\nIngresa solo valores positivos \nIntenta nuevamente')
-    time.sleep(1)
-
+    mensaje_error()    
+    loading_text('\nIngresa solo valores positivos \nIntenta nuevamente\n')
+    pausa()
 
 def dots():
-    dots = '\n....\n'
-    for i in range(len(dots)):
-        print(dots[i], sep=' ', end=' ')
-        time.sleep(0.3)
+    loading_text('\n....\n',0.3)
 
 def opcion_invalida():
-    error_msj()
-    print('\n*** Opción inválida ***\nTeclea solamente "1" o "0"')
-    time.sleep(1)
+    mensaje_error()
+    loading_text('\n*** Opción inválida ***\nTeclea solamente "1" o "0"\n')
+    pausa()
